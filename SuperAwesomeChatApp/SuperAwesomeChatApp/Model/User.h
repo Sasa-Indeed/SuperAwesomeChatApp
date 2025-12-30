31871e213a1881c6488e7c8d2161c97c8913b1dc
Zaituny <mohamedinv.580@gmail.com>
2025-12-30 21:47:36 +0200

    Added constructor to User class

--- SuperAwesomeChatApp/SuperAwesomeChatApp/Model/User.h
+++ SuperAwesomeChatApp/SuperAwesomeChatApp/Model/User.h
@@ -15,5 +15,18 @@ private:
 	std::string EmailAddress;
 	std::string About;
 	UserStatus status;
+public:
+	User(long id,
+		std::string FirstName,
+		std::string LastName,
+		std::string EmailAddress,
+		std::string About,
+		UserStatus status) : id(id),
+		                     FirstName(FirstName),
+		                     LastName(LastName),
+		                     EmailAddress(EmailAddress),
+		                     About(About),
+		                     status(status) {
+	}
 };
 
