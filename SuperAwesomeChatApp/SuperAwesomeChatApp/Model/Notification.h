86b851ffb693e95f135deefd1b0fa915837db04c
Zaituny <mohamedinv.580@gmail.com>
2025-12-30 21:47:47 +0200

    Added constructor to Notification class

--- SuperAwesomeChatApp/SuperAwesomeChatApp/Model/Notification.h
+++ SuperAwesomeChatApp/SuperAwesomeChatApp/Model/Notification.h
@@ -9,5 +9,17 @@ private:
 	long userID;
 	long messageID;
 	bool isRead;
+public:
+	Notification(long id,
+		std::string content,
+		long userID,
+		long messageID,
+		bool isRead) : id(id),
+		               content(content), 
+		               userID(userID),
+		               messageID(messageID),
+		               isRead(isRead) {
+
+	}
 };
 
