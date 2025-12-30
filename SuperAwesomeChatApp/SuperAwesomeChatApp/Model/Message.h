c17e490293a0cc01d4b15de0a20ecf29db875cec
Zaituny <mohamedinv.580@gmail.com>
2025-12-30 21:47:24 +0200

    Added constructor to Message class

--- SuperAwesomeChatApp/SuperAwesomeChatApp/Model/Message.h
+++ SuperAwesomeChatApp/SuperAwesomeChatApp/Model/Message.h
@@ -18,6 +18,20 @@ private:
 	bool isEdited;
 	time_t timeStamp;
 	MessageStatus status;
-
+public:
+	Message(long id,
+		std::string content,
+		long senderID,
+		long chatID,
+		bool isEdited,
+		time_t timeStamp,
+		MessageStatus status) : id(id),
+		                        content(content),
+		                        senderID(senderID),
+		                        chatID(chatID),
+		                        isEdited(isEdited),
+		                        timeStamp(timeStamp),
+		                        status(status){
+	}
 };
 
